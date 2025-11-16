06214491b68798f35bbaf8de22c64ff278e25915
abdr60699 <abdr60699@gmail.com>
2025-11-16 17:45:44 +0530

    feature test

---
+++ feature_test/apiclient/android/app/src/main/kotlin/com/example/apiclient/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.apiclient
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ feature_test/applock/android/app/src/main/kotlin/com/example/applock/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.applock
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
